2f2daeaca5d6d2cf237b2f77a93fe57731e7b762
Isa Robertini <robertiniisa@gmail.com>
2023-11-07 10:01:56 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// App Component Explanation
+// App Component Explanation 
 // The App component serves as a user interface for generating and downloading QR codes, utilizing the custom hook useQRCodeGenerator which encapsulates the logic for QR code generation and management. When rendered, the component displays a title ("Technigo QR Code Generator") and conditionally renders either an input field and a "Generate" button or a generated QR code image, a "Download" button, and a "Repeat" button, based on the showInput state variable. If showInput is true, users can input a URL and generate a QR code by clicking the "Generate" button. Once generated, the input field and "Generate" button are replaced by the QR code image and additional buttons. The "Download" button triggers a download of the QR code image, and the "Repeat" button resets the UI to allow for generating a new QR code. The url, setUrl, qr, showInput, generateQRCode, downloadQRCode, and repeatAction variables and functions are derived from the useQRCodeGenerator hook, providing the necessary state and actions to manage the QR code generation process.
 import logo from "./assets/technigo-logo.svg";
 // Import the custom hook useQRCodeGenerator
@@ -13,6 +13,7 @@ export const App = () => {
     <div className="">
       {/* Render the title */}
       <img className="logo" src={logo} alt="" />
+      <h1>this is a test</h1>
       <h1>Technigo QR Code Generator</h1>
       <p>Start Here</p>
       <QrExample />
