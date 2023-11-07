c8a21668473d4a793c5fd954342c1cc76190bf65
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-07 19:24:42 +0100

    Installed Tailwind and added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 // App Component Explanation
 // The App component serves as a user interface for generating and downloading QR codes, utilizing the custom hook useQRCodeGenerator which encapsulates the logic for QR code generation and management. When rendered, the component displays a title ("Technigo QR Code Generator") and conditionally renders either an input field and a "Generate" button or a generated QR code image, a "Download" button, and a "Repeat" button, based on the showInput state variable. If showInput is true, users can input a URL and generate a QR code by clicking the "Generate" button. Once generated, the input field and "Generate" button are replaced by the QR code image and additional buttons. The "Download" button triggers a download of the QR code image, and the "Repeat" button resets the UI to allow for generating a new QR code. The url, setUrl, qr, showInput, generateQRCode, downloadQRCode, and repeatAction variables and functions are derived from the useQRCodeGenerator hook, providing the necessary state and actions to manage the QR code generation process.
 import logo from "./assets/technigo-logo.svg";
-// Import the custom hook useQRCodeGenerator
-import { QrExample } from "./components/QrExample";
 
-// Define the App component
 export const App = () => {
-  // Destructure variables, properties and methods from the useQRCodeGenerator hook that you imported above here :)
-
-  // Return the JSX to render the component
-  return (
-    <div className="">
-      {/* Render the title */}
-      <img className="logo" src={logo} alt="" />
-      <h1>Technigo QR Code Generator</h1>
-      <p>Start Here</p>
-      <QrExample />
-
-      {/* Conditionally render based on wether the user is inputting an URL to generate a QR Code or the user wnats to downaload the generated QR Code from the url input */}
-      {/* {yourReactiveVariableThatTogglesTheDownloadQrCcodeOrInputField ? () : ()} */}
-    </div>
-  );
+    return (
+        <div className="bg-slate-700 min-h-full">
+            <div className="max-w-2xl mx-auto py-8 px-8">
+                <img
+                    src={logo}
+                    className="w-24 mb-8"
+                    alt="Technigo's logo"
+                />
+                <label className="block mb-4 text-center text-slate-300" htmlFor="input-url">
+                    Put in your url that you want to convert to a QR-code below:
+                </label>
+                <div className="flex">
+                    <input
+                        id="input-url"
+                        placeholder="https:// ..."
+                        className="border-2 border-e-0 border-slate-600 rounded-tl-xl rounded-bl-xl py-2 px-4 w-full focus:border-slate-500 focus:outline-none bg-slate-800" />
+                    <button
+                        className="text-slate-300 border-2 border-s-0 border-slate-600 rounded-tr-xl rounded-br-xl py-2 px-4 bg-slate-500"
+                    >
+                        Generate
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
--- src/hooks/useQRCodeGenerator.jsx
+++ src/hooks/useQRCodeGenerator.jsx
@@ -4,75 +4,75 @@ import QRCode from "qrcode";
 
 // Define a custom hook named useQRCodeGenerator
 export const useQRCodeGenerator = () => {
-  // Reactive State variable to store the input URL
-  //   const ...
+    // Reactive State variable to store the input URL
+    //   const ...
 
-  // Reactive State variable to store the generated QR code data URL
-  //   const ...
+    // Reactive State variable to store the generated QR code data URL
+    //   const ...
 
-  // Reactive State variable to toggle the visibility of the input element - boolean value
-  //   const ...
+    // Reactive State variable to toggle the visibility of the input element - boolean value
+    //   const ...
 
-  // Function to generate a QR code from the input URL
-  const generateQRCode = () => {
-    // HINT 1: Utilize the qrccode library that converts a URL to a QR code data URL.
-    // Use the Import of the qrcode and chain to the native method toDataUrl() much like the example provided and specify the data within the object that you will be passing such as the {url, {width, margin, color:{dark, light}}} which containes the information to generate the qr-code and url. Lastly, this native method toDataUrl() will contain a callback function  that will update the qr variable and will also update the variable toggling the visibility of the input element.
-    QRCode.toDataURL(
-      console.log("Delete This Line OR Comment Out")
-      // HINT 2: Ensure to pass the necessary parameters to the QR code generation method, such as the URL to convert and any styling options.
-      // ...
-      // HINT 3: Handle the callback of the QR code generation method, which provides the generated QR code data URL.
-      // ...
-      // HINT 4: Implement error handling to manage any issues that might occur during QR code generation.
-      // ...
-      // HINT 5: Update the relevant state variables with the generated QR code data URL and adjust the UI accordingly.
-      // ...
-      // HINT 6: Consider the user experience and how the UI should change once the QR code has been generated.
-      // ...
-    );
-  };
-
-  // Function to download the generated QR code as a PNG file
-  const downloadQRCode = () => {
-    // HINT 1: Consider encapsulating the filename prompting logic into a separate function.
-    const getFileName = () => {
-      // HINT 2: Use a method to prompt the user for input and store the response.
-      // ...
-      // HINT 3: Implement a check for an empty filename and utilize recursion to re-prompt the user if necessary.
-      // ...
-      // HINT 4: Ensure the function returns the obtained filename.
-      // ...
+    // Function to generate a QR code from the input URL
+    const generateQRCode = () => {
+        // HINT 1: Utilize the qrccode library that converts a URL to a QR code data URL.
+        // Use the Import of the qrcode and chain to the native method toDataUrl() much like the example provided and specify the data within the object that you will be passing such as the {url, {width, margin, color:{dark, light}}} which containes the information to generate the qr-code and url. Lastly, this native method toDataUrl() will contain a callback function  that will update the qr variable and will also update the variable toggling the visibility of the input element.
+        QRCode.toDataURL(
+            console.log("Delete This Line OR Comment Out")
+            // HINT 2: Ensure to pass the necessary parameters to the QR code generation method, such as the URL to convert and any styling options.
+            // ...
+            // HINT 3: Handle the callback of the QR code generation method, which provides the generated QR code data URL.
+            // ...
+            // HINT 4: Implement error handling to manage any issues that might occur during QR code generation.
+            // ...
+            // HINT 5: Update the relevant state variables with the generated QR code data URL and adjust the UI accordingly.
+            // ...
+            // HINT 6: Consider the user experience and how the UI should change once the QR code has been generated.
+            // ...
+        );
     };
 
-    // HINT 5: Call the above function to retrieve a filename and store it in a variable.
-    // ...
+    // Function to download the generated QR code as a PNG file
+    const downloadQRCode = () => {
+        // HINT 1: Consider encapsulating the filename prompting logic into a separate function.
+        const getFileName = () => {
+            // HINT 2: Use a method to prompt the user for input and store the response.
+            // ...
+            // HINT 3: Implement a check for an empty filename and utilize recursion to re-prompt the user if necessary.
+            // ...
+            // HINT 4: Ensure the function returns the obtained filename.
+            // ...
+        };
 
-    // HINT 6: Format the filename to ensure it is filesystem-friendly.
-    // ...
+        // HINT 5: Call the above function to retrieve a filename and store it in a variable.
+        // ...
 
-    // HINT 7: Create an anchor element to facilitate the download.
-    // ...
+        // HINT 6: Format the filename to ensure it is filesystem-friendly.
+        // ...
 
-    // HINT 8: Set the necessary attributes on the anchor element to prepare it for download.
-    // ...
+        // HINT 7: Create an anchor element to facilitate the download.
+        // ...
 
-    // HINT 9: Append the anchor element to the document to make it interactable.
-    // ...
+        // HINT 8: Set the necessary attributes on the anchor element to prepare it for download.
+        // ...
 
-    // HINT 10: Programmatically trigger a click on the anchor element to initiate the download.
-    // ...
+        // HINT 9: Append the anchor element to the document to make it interactable.
+        // ...
 
-    // HINT 11: Remove the anchor element from the document after the download has been initiated.
-    // ...
-  };
+        // HINT 10: Programmatically trigger a click on the anchor element to initiate the download.
+        // ...
 
-  // Function to reset the state and allow generating a new QR code
-  const repeatAction = () => {
-    // Reset the url state to an empty string
-    // Reset the qr state to an empty string
-    // Show the input element back to true :)
-  };
+        // HINT 11: Remove the anchor element from the document after the download has been initiated.
+        // ...
+    };
 
-  // Return the state variables and functions to be used in the component
-  return {};
+    // Function to reset the state and allow generating a new QR code
+    const repeatAction = () => {
+        // Reset the url state to an empty string
+        // Reset the qr state to an empty string
+        // Show the input element back to true :)
+    };
+
+    // Return the state variables and functions to be used in the component
+    return {};
 };
