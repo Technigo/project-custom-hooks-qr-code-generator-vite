b91ab2003d52d5cf273780db37e87d94852fe00c
mariateresepettersson <mariateresepettersson>
2023-11-08 19:36:29 +0000

    Background gradient and general styling to make responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const AppContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   font-family: 'Lexend', sans-serif;
-  color: #725AC1;
+  color: #F5ECE2;
   font-weight: 400;
   margin: 50px 20px;
   gap: 30px;
@@ -20,18 +20,57 @@ h1 {
   font-size: 36px;
   font-weight: 900;
   text-align: center;
-  margin: 20px 0 0 0;
+  margin: 0;
   padding: 0 0 30px 0;
+}
+
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
 }
 
 .button-wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 20px;
 }
 
-img {
+.qr-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  margin-top: 20px;
+}
+
+.qr-wrapper img {
   border-radius: 10px;
+  height: 300px;
+  width: 300px;
+}
+
+@media (min-width: 1280px) {
+  h1 {
+  font-size: 48px;
+  font-weight: 900;
+  text-align: center;
+  margin: 0;
+  padding: 0 0 30px 0;
+}
+
+.input-wrapper {
+  gap: 30px;
+}
+
+  .button-wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 30px;
+}
 }
 `;
 
@@ -41,22 +80,43 @@ const Input = styled.input`
   border: 1px solid #725AC1;
   border-radius: 5px;
   padding: 5px;
-  width: 300px;
+  width: 250px;
+
+  &:focus {
+    border-color: 5px solid #8D86C9; /* Change this to the desired color */
+  }
+
+  @media (max-width: 1279px) and (min-width: 744px) {
+    font-size: 24px;
+    width: 350px;
+  }
+
+  @media (min-width: 1280px) {
+    font-size: 24px;
+    width: 400px;
+  }
 `;
 
 const Button = styled.button`
-  background-color:#725AC1;
-  color: #fff;
+  background-color: #5ECBF5;
+    color: #fff;
   font-weight: 600;
   padding: 10px 20px;
   border: none;
   border-radius: 15px;
   font-size: 18px;
   cursor: pointer;
-  margin: 10px;
 
-  :hover {
-    background-color: #3321A6;
+  &:hover {
+    background-color:#F5ECE2;
+    color: #3D1050;
+  }
+  @media (max-width: 1279px) and (min-width: 744px) {
+    font-size: 24px;
+  }
+
+  @media (min-width: 1280px) {
+    font-size: 24px;
   }
 `;
 
@@ -75,34 +135,50 @@ export const App = () => {
     repeatAction
   } = useQRCodeGenerator();
 
+  //Lottie properties
+  const options = {
+    animationData: qranimation,
+    style: {
+      height: 300
+    },
+    loop: true,
+    autoplay: true
+  }
+
   // Return the JSX to render the component
   return (
     <AppContainer>
-      {/* Render the title */}
-      <h1>QR Code Generator</h1>
-      {/* Conditionall rendering of elements */}
       {isInputVisible ? (
         //Renders the input field and generate button
         <>
-          <Lottie animationData={qranimation} />
-          <Input
-            type="text"
-            ref={inputRef}
-            placeholder="e.g. https://google.com"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
+          <Lottie
+            animationData={options.animationData}
+            style={options.style}
+            loop={options.loop}
+            autoplay={options.autoplay}
           />
-          <Button onClick={generateQRCode}>Generate QR</Button>
+          <div className="input-wrapper">
+            <h1>QR Code Generator</h1>
+            <Input
+              type="text"
+              ref={inputRef}
+              placeholder="e.g. https://google.com"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+            <Button onClick={generateQRCode}>Generate QR</Button>
+          </div>
         </>
       ) : (
         //Renders the QR code, dowload button and repeat button
-        <>
+        <div className="qr-wrapper">
+          <h1>QR Code Generator</h1>
           <img src={qrCode} />
           <div className="button-wrapper">
             <Button onClick={downloadQRCode}>Download</Button>
             <Button onClick={repeatAction}>New QR</Button>
           </div>
-        </>
+        </div>
       )}
     </AppContainer>
   );
