64a0d442be79fd4d1edb0f111a96d06d6338126c
Hang-Nguyen-Vu <nguyenvuhang@gmail.com>
2023-11-08 16:22:22 +0100

    finish hook app and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,50 @@
 // App Component Explanation
 // The App component serves as a user interface for generating and downloading QR codes, utilizing the custom hook useQRCodeGenerator which encapsulates the logic for QR code generation and management. When rendered, the component displays a title ("Technigo QR Code Generator") and conditionally renders either an input field and a "Generate" button or a generated QR code image, a "Download" button, and a "Repeat" button, based on the showInput state variable. If showInput is true, users can input a URL and generate a QR code by clicking the "Generate" button. Once generated, the input field and "Generate" button are replaced by the QR code image and additional buttons. The "Download" button triggers a download of the QR code image, and the "Repeat" button resets the UI to allow for generating a new QR code. The url, setUrl, qr, showInput, generateQRCode, downloadQRCode, and repeatAction variables and functions are derived from the useQRCodeGenerator hook, providing the necessary state and actions to manage the QR code generation process.
-import logo from "./assets/technigo-logo.svg";
 // Import the custom hook useQRCodeGenerator
 import { useQRCodeGenerator } from "./hooks/useQRCodeGenerator";
 
 // Define the App component
 export const App = () => {
   // Destructure variables, properties and methods from the useQRCodeGenerator hook that you imported above here :)
+  const { 
+    url, 
+    qrcode, 
+    inputVisible, 
+    setUrl, 
+    generateQRCode, 
+    downloadQRCode, 
+    repeatAction 
+  } = useQRCodeGenerator();
+
+  //Handle input change
+  const handleInputChange = (e) => {
+    setUrl(e.target.value);
+  }
 
   // Return the JSX to render the component
   return (
-    <div className="">
-      {/* Render the title */}
-      <img className="logo" src={logo} alt="" />
-      <h1>Technigo QR Code Generator</h1>
-      <p>Start Here</p>
-      
-      {/* Conditionally render based on wether the user is inputting an URL to generate a QR Code or the user wnats to downaload the generated QR Code from the url input */}
-      {/* {yourReactiveVariableThatTogglesTheDownloadQrCcodeOrInputField ? () : ()} */}
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      {/* Conditionally render based on wether the user is inputting an URL to generate a QR Code or the user wants to download the generated QR Code from the url input */}
+      {inputVisible ? (
+        <div className="generate">
+          <input
+            type="text"
+            placeholder="e.g. https:/google.com"
+            value={url}
+            onChange={handleInputChange}
+          />
+          <button onClick={generateQRCode}>Generate</button>
+        </div>
+      ) : null}
+
+      {qrcode && 
+        <div className="download">
+          <img src={qrcode} />
+          <button onClick={downloadQRCode}>Download</button>
+          <button onClick={repeatAction}>Generate New</button>
+        </div>
+      }
     </div>
   );
 };
--- src/hooks/useQRCodeGenerator.jsx
+++ src/hooks/useQRCodeGenerator.jsx
@@ -9,41 +9,40 @@ export const useQRCodeGenerator = () => {
   const [url, setUrl] = useState("");
 
   // Reactive State variable to store the generated QR code data URL
-  const [qr, setQr] = useState("");
+  const [qrcode, setQrcode] = useState("");
 
   // Reactive State variable to toggle the visibility of the input element - boolean value
-  const [inputVisible, setInputVisible] = useState(false);
+  const [inputVisible, setInputVisible] = useState(true);
 
   // Function to generate a QR code from the input URL
   const generateQRCode = () => {
     // HINT 1: Utilize the qrccode library that converts a URL to a QR code data URL.
     // Use the Import of the qrcode and chain to the native method toDataUrl() much like the example provided and specify the data within the object that you will be passing such as the {url, {width, margin, color:{dark, light}}} which containes the information to generate the qr-code and url. Lastly, this native method toDataUrl() will contain a callback function  that will update the qr variable and will also update the variable toggling the visibility of the input element.
     QRCode.toDataURL(
-      // HINT 2: Ensure to pass the necessary parameters to the QR code generation method, such as the URL to convert and any styling options.
+      // HINT 2: Ensure to pass the necessary parameters to the QR code generation method, such as the URL to convert and any styling options.      
+      // HINT 3: Handle the callback of the QR code generation method, which provides the generated QR code data URL.
       url,
       {
-        width: 150,
-        margin: 10,
+        width: 300,
+        margin: 2,
         color: {
-          dark: darkgreen,
-          light: lightgrey
-        }
-      },
-      (err, url) => {
-        if (err) return console.error(err);
-        setQr(url);
-        setInputVisible(true);
-      }
-
-
-      // HINT 3: Handle the callback of the QR code generation method, which provides the generated QR code data URL.
-      // ...
+          dark: "#335383FF",
+          light: "#EEEEEEFF"
+        },
+      },      
       // HINT 4: Implement error handling to manage any issues that might occur during QR code generation.
-      // ...
-      // HINT 5: Update the relevant state variables with the generated QR code data URL and adjust the UI accordingly.
-      // ...
+      (err, url) => {
+        if (err) {
+          console.error("Error generating QR code:", err);
+          return;
+        }
+      // HINT 5: Update the relevant state variables with the generated QR code data URL and adjust the UI accordingly. 
       // HINT 6: Consider the user experience and how the UI should change once the QR code has been generated.
-      // ...
+        setQrcode(url);
+        setInputVisible(false);
+      },
+
+      
     );
   };
 
@@ -52,42 +51,55 @@ export const useQRCodeGenerator = () => {
     // HINT 1: Consider encapsulating the filename prompting logic into a separate function.
     const getFileName = () => {
       // HINT 2: Use a method to prompt the user for input and store the response.
-      // ...
+      const fileName = prompt("Please enter a name for the QR code download");
       // HINT 3: Implement a check for an empty filename and utilize recursion to re-prompt the user if necessary.
-      // ...
+      if (fileName === "") {
+        prompt("Please enter a file name: ");
+      }
       // HINT 4: Ensure the function returns the obtained filename.
-      // ...
+      return fileName;
     };
 
     // HINT 5: Call the above function to retrieve a filename and store it in a variable.
-    // ...
-
     // HINT 6: Format the filename to ensure it is filesystem-friendly.
-    // ...
+    const formattedFileName = getFileName().toLowerCase();
 
+  
     // HINT 7: Create an anchor element to facilitate the download.
-    // ...
+    const anchor = document.createElement("a");
 
     // HINT 8: Set the necessary attributes on the anchor element to prepare it for download.
-    // ...
+    anchor.href = {url};
+    anchor.download = `${formattedFileName}.png`;
 
     // HINT 9: Append the anchor element to the document to make it interactable.
-    // ...
+    document.body.appendChild(anchor);
 
     // HINT 10: Programmatically trigger a click on the anchor element to initiate the download.
-    // ...
+    anchor.click();
 
     // HINT 11: Remove the anchor element from the document after the download has been initiated.
-    // ...
+    document.body.removeChild(anchor);
   };
 
   // Function to reset the state and allow generating a new QR code
   const repeatAction = () => {
     // Reset the url state to an empty string
+    setUrl("");
     // Reset the qr state to an empty string
+    setQrcode("");
     // Show the input element back to true :)
+    setInputVisible(true);
   };
 
   // Return the state variables and functions to be used in the component
-  return {};
+  return {
+    url,
+    qrcode,
+    inputVisible,
+    setUrl,
+    generateQRCode,
+    downloadQRCode,
+    repeatAction  
+  };
 };
