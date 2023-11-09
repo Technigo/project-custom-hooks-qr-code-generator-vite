cf2c394a14b272aaa3bd2ee94fc0a9335a923f1a
Sakura Tanaka <sito6496t@gmail.com>
2023-11-09 22:50:28 +0100

    test

--- src/components/Three/Circle.tsx
+++ src/components/Three/Circle.tsx
@@ -5,29 +5,23 @@ import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 function Circle(props: ThreeElements["mesh"]) {
   const meshRef = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
+
   const [active, setActive] = useState(false);
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  useFrame((state, delta) => (meshRef.current.rotation.y += delta));
   const colorMap = useLoader(TextureLoader, "/earth.jpg");
 
   return (
     <>
-      <spotLight args={["#000", 1, 10, 10]} />
+      <spotLight args={["#ffffff", 10, 1000, 50]} />
       <mesh
         {...props}
         ref={meshRef}
         scale={active ? 0.5 : 0.3}
         onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
       >
-        {/* <directionalLight /> */}
-
         <ambientLight intensity={1} />
         <sphereGeometry args={[3, 32, 16]} />
-        {/* <torusKnotGeometry args={[1, 0.4, 64, 8, 2, 3]} /> */}
         <meshStandardMaterial
-          // color={hovered ? "rgb(12, 74, 110)" : "pink"}
           displacementScale={0.2}
           transparent
           attach="material"
