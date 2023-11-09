6b5136ae649ad76b4795c1f90150c9ab0f65f145
veronica astrom <v.aastrom@gmail.com>
2023-11-09 12:42:15 +0100

    updated error handling, added focus on the input field when the page is mounted as well as after the repeat button is pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App Component Explanation
 // The App component serves as a user interface for generating and downloading QR codes, utilizing the custom hook useQRCodeGenerator which encapsulates the logic for QR code generation and management. When rendered, the component displays a title ("Technigo QR Code Generator") and conditionally renders either an input field and a "Generate" button or, a generated QR code image, a "Download" button, and a "Repeat" button, based on the showInput state variable. If showInput is true, users can input a URL and generate a QR code by clicking the "Generate" button. Once generated, the input field and "Generate" button are replaced by the QR code image and additional buttons. The "Download" button triggers a download of the QR code image, and the "Repeat" button resets the UI to allow for generating a new QR code. The url, setUrl, qr, showInput, generateQRCode, downloadQRCode, and repeatAction variables and functions are derived from the useQRCodeGenerator hook, providing the necessary state and actions to manage the QR code generation process.
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useQRCodeGenerator } from "./hooks/useQRCodeGenerator";
 
 export const App = () => {
@@ -12,25 +12,22 @@ export const App = () => {
     isInputVisible,
     generateQRCode,
     downloadQRCode,
-    repeatAction
+    repeatAction,
+    inputRef,
   } = useQRCodeGenerator();
-
-  // Create a ref for the input element
-  const inputRef = useRef(null);
 
   // Use useEffect to focus on the input element when the component mounts
   useEffect(() => {
-    if (inputRef.current) {
+    if (inputRef.current && isInputVisible) {
       inputRef.current.focus();
     }
-  }, []);
+  }, [isInputVisible, inputRef]);
 
   // Handle "Enter" key press in the input field
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
       // Prevent the default form submission
       event.preventDefault();
-      // Trigger the generateQRCode function
       generateQRCode();
     }
   };
@@ -53,7 +50,7 @@ export const App = () => {
         </div>
       ) : (
         <div>
-          <img src={qr} alt="QR code" />
+          <img src={qr} alt={`QR code for ${url}`}/>
           <button onClick={downloadQRCode}>Download</button>
           <button onClick={repeatAction}>Repeat</button>
         </div>
--- src/hooks/useQRCodeGenerator.jsx
+++ src/hooks/useQRCodeGenerator.jsx
@@ -1,65 +1,86 @@
 // Hook Explanation
 // This React component, specifically a custom hook named useQRCodeGenerator, is designed to facilitate the generation and downloading of QR codes. Initially, it utilizes the useState hook from React to manage three pieces of state: url (to store the input URL that will be converted into a QR code), qr (to store the generated QR code data URL), and showInput (a boolean to toggle the visibility of an input element in the UI). The hook exposes a method generateQRCode which utilizes the QRCode.toDataURL method to convert the provided URL into a QR code, applying specific styling options, and then updates the state with the generated QR code and hides the input. The downloadQRCode method allows users to download the generated QR code as a PNG file, prompting them to provide a filename and handling the download process via creating an anchor element in the DOM. Lastly, the repeatAction method resets the state to allow users to generate a new QR code. The hook returns an object containing the state variables and methods, enabling them to be utilized in the component where the hook is used.
 import QRCode from "qrcode";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-// Define a custom hook named useQRCodeGenerator
+// Custom hook for a QR Code Generator
 export const useQRCodeGenerator = () => {
-  // State variable to store the input URL
-  const [url, setUrl] = useState("");
 
-  // State variable to store the generated QR code data URL
-  const [qr, setQr] = useState("");
+  const [url, setUrl] = useState(""); // State variable to store the input URL
+  const [qr, setQr] = useState("");   // State variable to store the generated QR code data URL
+  const [isInputVisible, setIsInputVisible] = useState(true); // State variable to toggle the visibility of the input element
 
-  // State variable to toggle the visibility of the input element
-  const [isInputVisible, setIsInputVisible] = useState(true);
+  const inputRef = useRef(null);  // Ref used to interact with the input element
+
+  const DEFAULT_FILE_FORMAT = "png"; // Default file format. Can be changed to other formats if needed.
+
+
+  // Function to focus on the input element
+  const focusInput = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };  
 
   // Function to generate a QR code from the input URL
   const generateQRCode = () => {
-    QRCode.toDataURL(
-      url,
-      {
-        width: 100,
-        margin: 2,
-        color: {
-          dark: "#335383FF",
-          light: "#EEEEEEFF",
+
+    if (url.trim() === "") {
+      alert("Please enter a valid input.");
+
+    } else {
+      QRCode.toDataURL(
+        url,  // The data to be encoded as a QR code (e.g., a URL)
+        {
+          width: 200,  // Width of the QR code
+          margin: 2,   // Margin around the QR code. Increasing this shrinks the QR code and increases the size of the background of the image in order to fit within the width. Keep this small.
+          color: {
+            dark: "#335383FF",   // The darker colorr in the QR code image
+            light: "#EEEEEEFF",  // Color of the background in the QR code
+          },
         },
-      },
-      (error, url) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(url);
-          setQr(url);
-          setIsInputVisible(false);
+        (error, url) => {
+          if (error) {
+            console.error(error);
+          } else {
+            console.log(url);
+            setQr(url);
+            setIsInputVisible(false);
+          }
         }
-      }
-    );
+      );
+    }
   };
 
   // Function to prompt the user for a filename
   const getFileName = () => {
     const fileName = prompt('Enter a filename for the QR code:');
+
     if (fileName === null) {
-      // User canceled the action, do nothing or handle accordingly
+      // User canceled the action. The value returned is null. If I use return; here, something like "let fileName = getFileName();" will fail since there's no value to assign. For chrome this leads to an undefined file being downloaded. return null; dosn't tell the program what to do if the fileName is null. What type of action the program needs to take is defined in if (fileName === null) {return;}, in this specific case. 
       return null;
-    } else if (fileName === "") {
+      // fileName.trim() === "" checks if the trimmed version of the filename is an empty string. If the filename consists only of spaces, tabs, or newline characters, the trim() method will remove them, and the condition will evaluate to true, indicating that the filename is empty.
+    } else if (fileName.trim() === "") {
       // No filename provided, retry
+      alert("Please enter a valid filename.");
       return getFileName();
     } else {
       return fileName;
     }
   };
 
+
   // Function to download the generated QR code as a PNG file
   const downloadQRCode = () => {
     let fileName = getFileName();
+
+    // if the fileName, which is from getFileName(), is null, then do nothing (return;) If I have return null here, I move the problem to the next part where downloadQRCode() is used, which is the button. That is not what I want so the action needs to be defined here.
     if (fileName === null) {
       return;
     }
-    let formattedFileName = fileName + ".png";
-    
+    const fileFormat = DEFAULT_FILE_FORMAT;
+    let formattedFileName = `${fileName}.${fileFormat}`;
+
     // Check if qr contains a valid data URL
     if (qr) {
       // Create the download link after the QR code data URL is available
@@ -79,6 +100,7 @@ export const useQRCodeGenerator = () => {
       document.body.removeChild(downloadLink);
     } else {
       console.error("QR code data is not available.");
+      alert("Error generating QR code. Please try again.");
     }
   };
 
@@ -87,10 +109,18 @@ export const useQRCodeGenerator = () => {
     setUrl("");
     setQr("");
     setIsInputVisible(true);
+    focusInput(); // Focus on the input element after repeating action
   };
 
   // Return the state variables and functions to be used in the component
   return {
-    url, setUrl, qr, isInputVisible, generateQRCode, downloadQRCode, repeatAction
+    url,
+    setUrl,
+    qr,
+    isInputVisible,
+    generateQRCode,
+    downloadQRCode,
+    repeatAction,
+    inputRef,
   };
 };
