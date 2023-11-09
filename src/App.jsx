0f763d53fd266bd82f9781c0b19ee2708359740d
Inna Kokic <inna@Innas-MacBook-Pro-2.local>
2023-11-09 20:51:04 +0100

    Added more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,66 @@
-// import logo from "./assets/technigo-logo.svg";
-// import {FiDownload} from 'react-icons/fa'
 import { useQRCodeGenerator } from "./hooks/useQRCodeGenerator";
 
 // Define the App component
 export const App = () => {
-   const { url, setUrl, showInput, qrCode, generateQRCode, downloadQRCode, repeatAction } = useQRCodeGenerator();
-  // Destructure variables, properties and methods from the useQRCodeGenerator hook that you imported above here :)
-
-  // Return the JSX to render the component
+   const { 
+    url, 
+    setUrl, 
+    showInput, 
+    qrCode, 
+    generateQRCode, 
+    downloadQRCode, 
+    repeatAction 
+  } = useQRCodeGenerator();
+  
   return (
   
-  <div className="app">
-    {/* Render the title */}
-    {/* <img className="logo" src={logo} alt="" /> */}
-    <h1>QR Code Generator</h1>
-    <p>Start Here</p>
+    <div className="app">
+      <header>
+          <h1>QR Code Generator</h1>
+      </header>
+      
+      {showInput ? ( 
+          <>
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)} 
+              placeholder="Enter your URL here.."
+            />
+            <button 
+            className="text-button" 
+            onClick={generateQRCode}>Generate</button>
+          </> ) : ( 
+          <> 
+            {qrCode && 
+              <div className="generated-wrapper">
 
-   
-     <div className="input-container">
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)} 
-          placeholder="Enter URL e.g. https://google.com"
-        />
-        <button className="generate-btn" onClick={generateQRCode}>Generate</button>
-      </div>
-   
-      <div className="qrCode-container">
-       {qrCode && <> <img src={qrCode} alt="Generated QR Code" />
-        <button className="download-btn" onClick={downloadQRCode}>Download QR code</button>
-        <button className="repeat-btn" onClick={repeatAction}>One more!</button>
-        </>}
-      </div>
-    
-  </div>
+                <h2>Qr code for {url}</h2>
+
+                <div className="qr-container">
+
+                  <img src={qrCode} alt="Generated QR Code" />
+                  
+                  <div className="qrCode-btn-container">
+                    <button className="Btn"
+                    onClick={downloadQRCode}>
+                    <svg className="svgIcon" viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path></svg>
+                    <span className="icon2"></span>
+                    <span className="tooltip">Download</span>
+                    </button>
+                    
+                    <button 
+                    className="text-button" 
+                    onClick={repeatAction}>Create one more</button>
+                  </div>
+
+                </div>
+              </div>
+            }
+          </> 
+        )}
+
+    </div>
 );
 
 };
--- src/hooks/useQRCodeGenerator.jsx
+++ src/hooks/useQRCodeGenerator.jsx
@@ -14,8 +14,8 @@ export const useQRCodeGenerator = () => {
       width: 200,
       margin: 2,
       color: {
-        light: "#FFFFFF",
-        dark:""
+        light: '#0000',
+        dark:'#ffff'
       }
     }, (err, url)=> {
       if (err) return console.log(err)
@@ -31,36 +31,31 @@ export const useQRCodeGenerator = () => {
   // Define a function to get the filename from the user through a prompt
   const getFileName = () => {
     let fileName = prompt("Enter a filename for the QR code", "qrcode"); // A prompt message with a placeholder
-    if (!fileName) { 
-      // Handle empty filename by recursively asking for a filename
-      fileName = getFileName();
-    }
     return fileName;
   };
 
-  // Call the getFileName function to get the desired filename from the user
+  // Calling the getFileName function to get the desired filename from the user
   const fileName = getFileName();
 
-  // Format the filename to ensure it is filesystem-friendly
-  const formattedFileName = fileName.replace(/\s+/g, "_").toLowerCase(); // Replace spaces with underscores and convert to lowercase
+  // Formatting the filename to ensure it is filesystem-friendly
+  const formattedFileName = fileName.replace(/\s+/g, "_").toLowerCase(); // Replaces spaces with underscores and convert to lowercase
 
-  // Create an anchor element to facilitate the download
+  // Creates an anchor element to facilitate the download
   const downloadLink = document.createElement("a");
 
-  // Set the necessary attributes on the anchor element to prepare it for download
-  downloadLink.href = qrCode; // Set the href to the generated QR code data URL
-  downloadLink.download = `${formattedFileName}.png`; // Set the download attribute with the formatted filename and .png extension
+  // Sets the necessary attributes on the anchor element to prepare it for download
+  downloadLink.href = qrCode; // Sets the href to the generated QR code data URL
+  downloadLink.download = `${formattedFileName}.png`; // Sets the download attribute with the formatted filename and .png extension
 
-  // Append the anchor element to the document to make it interactable
+  // Appends the anchor element to the document to make it interactable
   document.body.appendChild(downloadLink);
 
-  // Programmatically trigger a click on the anchor element to initiate the download
+  // Programmatically triggers a click on the anchor element to initiate the download
   downloadLink.click();
 
-  // Remove the anchor element from the document after the download has been initiated
+  // Removes the anchor element from the document after the download has been initiated
   document.body.removeChild(downloadLink);
 };
-
 
 
   // Function to reset the state and allow generating a new QR code
@@ -70,7 +65,7 @@ export const useQRCodeGenerator = () => {
     setShowInput(true);
   };
 
-  // Return the state variables and functions to be used in the component
+  // Returning the state variables and functions to be used in the app component
   return { url, setUrl, qrCode, showInput, generateQRCode, downloadQRCode, repeatAction };
 };
 
