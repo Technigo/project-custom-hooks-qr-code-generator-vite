65a3b6d62f4866061c748593696346340463dd94
Idah Collin <idah_collin@hotmail.com>
2023-11-09 11:14:52 +0100

    New test to make push work

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,202 @@
-// App Component Explanation
-// The App component serves as a user interface for generating and downloading QR codes, utilizing the custom hook useQRCodeGenerator which encapsulates the logic for QR code generation and management. When rendered, the component displays a title ("Technigo QR Code Generator") and conditionally renders either an input field and a "Generate" button or a generated QR code image, a "Download" button, and a "Repeat" button, based on the showInput state variable. If showInput is true, users can input a URL and generate a QR code by clicking the "Generate" button. Once generated, the input field and "Generate" button are replaced by the QR code image and additional buttons. The "Download" button triggers a download of the QR code image, and the "Repeat" button resets the UI to allow for generating a new QR code. The url, setUrl, qr, showInput, generateQRCode, downloadQRCode, and repeatAction variables and functions are derived from the useQRCodeGenerator hook, providing the necessary state and actions to manage the QR code generation process.
-import logo from "./assets/technigo-logo.svg";
-// Import the custom hook useQRCodeGenerator
-import { QrExample } from "./components/QrExample";
+import React, { useState } from 'react'
+import { useQRCodeGenerator } from './hooks/useQRCodeGenerator' // Import of the custom hook
+import styled from 'styled-components'
+import QRPlantImage from '/QRplant.jpg' // Import of image used via the styled component
 
-// Define the App component
-export const App = () => {
-  // Destructure variables, properties and methods from the useQRCodeGenerator hook that you imported above here :)
+// Styled components
+const AppMain = styled.div`
+background-color: #FFD2CD;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 100vh;
+text-align: center;
+`
 
-  // Return the JSX to render the component
+const QRPlant = styled.img`
+width: 60%;`
+
+
+const Titles = styled.div`
+  position: center;
+  display: inline-block;
+`
+
+const H1 = styled.h1`
+  color: #4B634D;
+  font-size: 2rem;
+  text-align: center;
+  margin: 5% 0%;
+`
+
+const H2 = styled.h2`
+  color: #4B634D;
+  font-size: 1,5rem;
+`
+
+const Button = styled.button`
+  background-color: #5F7761;
+  color: white;
+  padding: 10px 25px;
+  text-align: center;
+  font-size: 1rem;
+  margin: 2px;
+  cursor: pointer;
+  border-radius: 20px;
+  border: none;
+`
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`
+
+const InputField = styled.input`
+  padding: 8px;
+  margin-bottom: 5%;
+  border: 1px solid #ccc;
+`
+
+// Styling of error message if the URL isn't correct
+const ErrorMessage = styled.div`
+  color: #4B634D;
+  margin-top: 8px;
+  font-size: 14px;
+`
+
+const QRCodeImage = styled.img`
+  display: block;
+  margin: auto; /* This will center the image horizontally */
+  margin-top: 3%;
+  margin-bottom: 5%;
+`
+
+// Styling of the dialogbox that opens after QR-code is generated if user wants to download
+const DialogBox = styled.div`
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const DialogContent = styled.div`
+  background: white;
+  padding: 10%;
+`
+/* Tillbaka */
+
+// App component definition
+const App = () => {
+  // Using the custom hook to manage QR code generation logic
+  const { url, setUrl, qrCode, showInput, generateQRCode, resetQRCode } = useQRCodeGenerator()
+  // State variables managed by the component
+  const [showDialogbox, setShowDialogbox] = useState(false)
+  const [fileName, setFileName] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  // Function to validate the URL and generate the QR code
+  const handleGenerate = (e) => {
+    e.preventDefault();
+    const validUrlRegex = /^(https?:\/\/)?www\.[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+
+    if (validUrlRegex.test(url)) {
+      generateQRCode(url)
+      setErrorMessage('')
+    } else {
+      setErrorMessage("Oups! Enter a valid web address (hint: https://www...)")
+    }
+  }
+
+  // Function to handle the download of the generated QR-code when the modal/dialog box is "true" = open
+  const downloadQRCode = () => {
+    if (!fileName) {
+      setShowDialogbox(true)
+      return
+    }
+
+    const link = document.createElement('a')
+    link.href = qrCode
+    link.download = `${fileName}.png`
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+
+    // Reset after download
+    resetQRCode()
+    setFileName('')
+    setShowDialogbox(false)
+  }
+
+  // Function to reset
+  const repeatAction = () => {
+    resetQRCode() // This will clear the generated QR code and show the input again.
+  }
+
+  // Function to close the modal/dialog box
+  const closeDialogbox = () => setShowDialogbox(false)
+
   return (
-    <div className="">
-      {/* Render the title */}
-      <img className="logo" src={logo} alt="" />
-      <h1>Technigo QR Code Generator</h1>
-      <p>Start Here</p>
-      <QrExample />
+    <AppMain>
+      {/* Displaying elements with conditional rendering */}
+      <QRPlant src={QRPlantImage} alt="Plant" />
+      <Titles>
+        <H1>QR Code Generator</H1>
+      </Titles>
 
-      {/* Conditionally render based on wether the user is inputting an URL to generate a QR Code or the user wnats to downaload the generated QR Code from the url input */}
-      {/* {yourReactiveVariableThatTogglesTheDownloadQrCcodeOrInputField ? () : ()} */}
-    </div>
-  );
-};
+      {/* Form for entering URL and generating QR code */}
+      {showInput && (
+        <FormContainer onSubmit={handleGenerate}>
+          <InputField
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Enter URL here"
+            required
+          />
+          <Button type="submit">Generate QR-code</Button>
+          {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+        </FormContainer>
+      )}
+
+      {/* Displaying QR code and related buttons */}
+      {qrCode && (
+        <>
+          <QRCodeImage src={qrCode} alt="QR Code" />
+          <div>
+            <Button onClick={downloadQRCode}>Download QR-code</Button>
+            <Button onClick={repeatAction}>Reset</Button>
+          </div>
+        </>
+      )}
+
+      {/* Modal/Dialog box for providing a filename before download */}
+      {showDialogbox && (
+        <DialogBox>
+          <DialogContent>
+            <H2>Enter a file name for your QR Code</H2>
+            <input
+              type="text"
+              value={fileName}
+              onChange={(e) => setFileName(e.target.value)}
+              placeholder="Add name for file here"
+            />
+            <Button onClick={downloadQRCode}>Download</Button>
+            <Button onClick={closeDialogbox}>Cancel</Button>
+          </DialogContent>
+        </DialogBox>
+      )}
+    </AppMain>
+  )
+}
+
+export default App
