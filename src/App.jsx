bedf4c6d75490f5a5a6f590e8d132a6c48428204
Inna Kokic <inna@Innas-MacBook-Pro-2.local>
2023-11-09 16:08:30 +0100

    Started styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
-// App Component Explanation
-// The App component serves as a user interface for generating and downloading QR codes, utilizing the custom hook useQRCodeGenerator which encapsulates the logic for QR code generation and management. When rendered, the component displays a title ("Technigo QR Code Generator") and conditionally renders either an input field and a "Generate" button or a generated QR code image, a "Download" button, and a "Repeat" button, based on the showInput state variable. If showInput is true, users can input a URL and generate a QR code by clicking the "Generate" button. Once generated, the input field and "Generate" button are replaced by the QR code image and additional buttons. The "Download" button triggers a download of the QR code image, and the "Repeat" button resets the UI to allow for generating a new QR code. The url, setUrl, qr, showInput, generateQRCode, downloadQRCode, and repeatAction variables and functions are derived from the useQRCodeGenerator hook, providing the necessary state and actions to manage the QR code generation process.
-import logo from "./assets/technigo-logo.svg";
-// Import the custom hook useQRCodeGenerator
-// import { QrExample } from "./components/QrExample";
-
+// import logo from "./assets/technigo-logo.svg";
+// import {FiDownload} from 'react-icons/fa'
 import { useQRCodeGenerator } from "./hooks/useQRCodeGenerator";
 
 // Define the App component
 export const App = () => {
-   const { url, setUrl, qrCode, generateQRCode, downloadQRCode, repeatAction } = useQRCodeGenerator();
+   const { url, setUrl, showInput, qrCode, generateQRCode, downloadQRCode, repeatAction } = useQRCodeGenerator();
   // Destructure variables, properties and methods from the useQRCodeGenerator hook that you imported above here :)
 
   // Return the JSX to render the component
   return (
-  <div className="">
+  
+  <div className="app">
     {/* Render the title */}
-    <img className="logo" src={logo} alt="" />
-    <h1>Technigo QR Code Generator</h1>
+    {/* <img className="logo" src={logo} alt="" /> */}
+    <h1>QR Code Generator</h1>
     <p>Start Here</p>
 
    
-      <div>
+     <div className="input-container">
         <input
           type="text"
           value={url}
           onChange={(e) => setUrl(e.target.value)} 
-          placeholder="Enter URL"
+          placeholder="Enter URL e.g. https://google.com"
         />
-        <button onClick={generateQRCode}>Generate</button>
+        <button className="generate-btn" onClick={generateQRCode}>Generate</button>
       </div>
    
-    
-      <div>
-
+      <div className="qrCode-container">
        {qrCode && <> <img src={qrCode} alt="Generated QR Code" />
-        <button onClick={downloadQRCode}>Download</button>
-        <button onClick={repeatAction}>Repeat</button>
+        <button className="download-btn" onClick={downloadQRCode}>Download QR code</button>
+        <button className="repeat-btn" onClick={repeatAction}>One more!</button>
         </>}
       </div>
     
--- src/hooks/useQRCodeGenerator.jsx
+++ src/hooks/useQRCodeGenerator.jsx
@@ -1,47 +1,3 @@
-// import QRCode from "qrcode";
-// import { useState } from "react";
-
-// // Define a custom hook named useQRCodeGenerator
-// export const useQRCodeGenerator = () => {
-// const [url, setUrl] = useState("");
-// const [qrCode, setQrCode] = useState("");
-// const [showInput, setShowInput] = useState(true)
-
-
-//   // Function to generate a QR code from the input URL
-//   const generateQRCode = () => {
-    
-//     QRCode.toDataURL(url, (err, url)=> {
-//       if (err) return console.log(err)
-//       console.log(url)
-//     setQrCode(url)
-//     })
-
-//   };
-
-//   // Function to download the generated QR code as a PNG file
-//   const downloadQRCode = () => {
-    
-//     const getFileName = () => {
- 
-//     };
-
-   
-//   };
-
-//   // Function to reset the state and allow generating a new QR code
-//   const repeatAction = () => {
-   
-//   };
-
-//   // Return the state variables and functions to be used in the component
-//   return {
-    
-//   };
-// };
-
-// Hook Explanation
-// This React component, specifically a custom hook named useQRCodeGenerator, is designed to facilitate the generation and downloading of QR codes. Initially, it utilizes the useState hook from React to manage three pieces of state: url (to store the input URL that will be converted into a QR code), qr (to store the generated QR code data URL), and showInput (a boolean to toggle the visibility of an input element in the UI). The hook exposes a method generateQRCode which utilizes the QRCode.toDataURL method to convert the provided URL into a QR code, applying specific styling options, and then updates the state with the generated QR code and hides the input. The downloadQRCode method allows users to download the generated QR code as a PNG file, prompting them to provide a filename and handling the download process via creating an anchor element in the DOM. Lastly, the repeatAction method resets the state to allow users to generate a new QR code. The hook returns an object containing the state variables and methods, enabling them to be utilized in the component where the hook is used.
 import QRCode from "qrcode";
 import { useState } from "react";
 
@@ -54,10 +10,18 @@ export const useQRCodeGenerator = () => {
   // Function to generate a QR code from the input URL
   const generateQRCode = () => {
     
-    QRCode.toDataURL(url, (err, url)=> {
+    QRCode.toDataURL(url, {
+      width: 200,
+      margin: 2,
+      color: {
+        light: "#FFFFFF",
+        dark:""
+      }
+    }, (err, url)=> {
       if (err) return console.log(err)
       console.log(url)
     setQrCode(url)
+    setShowInput(false)
     })
 
   };
