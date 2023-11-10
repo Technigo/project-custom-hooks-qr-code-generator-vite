1abe40415cea2d31885a60ad50e738873cf076d7
majoh23 <139242342+majoh23@users.noreply.github.com>
2023-11-10 10:44:10 +0100

    README

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+// Import the custom hook
 import { useQRCodeGenerator } from "./hooks/useQRCodeGenerator";
 
+// Define the component
 export const App = () => {
+  // Destructure values custom hook
   const { url, setUrl, qr, showInput, generateQRCode, downloadQRCode, repeatAction } = useQRCodeGenerator();
 
+  // Render the App component
   return (
     <div className="app">
       <h1>QR Code Generator</h1>
 
+      {/* Conditionally render input or QR code based on state */}
       {showInput ? (
         <div className='input-wrapper'>
+          {/* Input field for URL */}
           <input
             id="urlInput"
             name="url"
@@ -16,13 +22,17 @@ export const App = () => {
             onChange={(e) => setUrl(e.target.value)}
             placeholder="Enter URL"
           />
+          {/* Button to trigger QR code generation */}
           <button onClick={generateQRCode}>Generate</button>
         </div>
       ) : (
         <div>
+          {/* Display the generated QR code image */}
           <img src={qr} alt="Generated QR Code" />
           <div className="button-wrapper">
+            {/* Button to download the generated QR code */}
             <button onClick={downloadQRCode}>Download</button>
+            {/* Button to restart the QR code generation process */}
             <button onClick={repeatAction}>Restart</button>
           </div>
         </div>
--- src/hooks/useQRCodeGenerator.jsx
+++ src/hooks/useQRCodeGenerator.jsx
@@ -1,23 +1,32 @@
+// Import QRCode library
 import QRCode from "qrcode";
+
+// Import the useState hook
 import { useState } from 'react';
 
+// Define a custom hook
 export const useQRCodeGenerator = () => {
+  //  Variables for URL, QR code data, and input
   const [url, setUrl] = useState('');
   const [qr, setQr] = useState('');
   const [showInput, setShowInput] = useState(true);
 
+  // Function to validate URL
   const isValidUrl = (input) => {
     const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
     return urlPattern.test(input);
   };
 
+  // Function to generate a QR code from the URL
   const generateQRCode = async () => {
     try {
+      // Validate the URL and show an alert if it's invalid
       if (!url.trim() || !isValidUrl(url)) {
         alert('Please enter a valid URL (http://www.example.com)');
         return;
       }
 
+      // Generate the QR code data URL
       const qrCodeDataUrl = await QRCode.toDataURL(url, {
         width: 500,
         margin: 3,
@@ -27,6 +36,7 @@ export const useQRCodeGenerator = () => {
         }
       });
 
+      // Set the generated QR code and hide the input
       setQr(qrCodeDataUrl);
       setShowInput(false);
     } catch (err) {
@@ -34,7 +44,9 @@ export const useQRCodeGenerator = () => {
     }
   };
 
+  // Function to download the generated QR as a file
   const downloadQRCode = () => {
+    // Function to get file name from the user
     const getFileName = () => {
       let filename = prompt('Please enter a file name', 'QRcode');
       if (!filename) {
@@ -43,22 +55,26 @@ export const useQRCodeGenerator = () => {
       return filename.replace(/[^a-z0-9]/gi, '_').toLowerCase();
     };
 
+    // Create filename and download link
     const filename = `${getFileName()}.png`;
-
     const link = document.createElement('a');
     link.href = qr;
     link.download = filename;
+
+    // Click the link and remove it from the document
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
+  // Function to reset the state
   const repeatAction = () => {
     setUrl('');
     setQr('');
     setShowInput(true);
   };
 
+  // Return the state variables and functions
   return {
     url,
     setUrl,
