a5af40d5f76175e234c8d610ffdaf56b96cc7048
Evelyn Del Carmen <hey@evelyndelcarmen.com>
2024-02-26 15:30:07 +0000

    the animation is in place and working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { useQRCodeGenerator } from "./hooks/useQRCodegenerator";
+import Lottie from "lottie-react";
+import QrAnimation from "./assets/lottieanimation/QrAnimation.json"
+import "./index.css";
+
+
 
 const App = () => {
   const {
@@ -14,7 +19,10 @@ const App = () => {
   // Make sure to return some JSX here for your component to render
   return (
     <div>
-      {/* Your component's JSX goes here */}
+      {/* Display the Lottie animation */}
+      <Lottie animationData={QrAnimation} loop autoplay style={{ width: 200, height: 200 }} />
+
+      {/* Additional UI elements like input fields, buttons, QR Code display, etc., go here */}
     </div>
   );
 };
